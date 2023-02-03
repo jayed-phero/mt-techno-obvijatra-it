8f92738f7e9f719c58fb638e09c88394308ba5d0
jayed-phero <jayed.programminghero@gmail.com>
2023-02-03 17:33:02 +0600

    commit

---
+++ src/Pages/Home/HomeBanner/im.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const im = () => {
+    return (
+        <div> <p class="text-lg text-para">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia consequatur
+        adipisci tenetur repudiandae rerum quos.
+    </p>
+        <div> <p class="text-lg text-para">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia consequatur
+        adipisci tenetur repudiandae rerum quos.
+    </p>
+        <div> <p class="text-lg text-para">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia consequatur
+        adipisci tenetur repudiandae rerum quos.
+    </p>
+        <div> <p class="text-lg text-para">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officia consequatur
+        adipisci tenetur repudiandae rerum quos.
+    </p>
+            
+        </div>
+    );
+};
+
+export default im;
